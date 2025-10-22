47ab3ecfbfa81efa4b7d1fedd3d3f09b9e3bf96d
Alex Yavorskyi <entertains1997@gmail.com>
2025-10-22 13:38:19 +0300

    start

--- src/js/main.js
+++ src/js/main.js
@@ -12,23 +12,24 @@ window.addEventListener("load", () => {
   const texts = ["Собираем орехи...", "Подготовка завершена!"];
   let textMaxWidth = 50;
   let index = 0;
-
-  preloadText.style.width = textMaxWidth + "px";
-  const textInterval = setInterval(() => {
-
-    preloadText.textContent = texts[index];
-    textMaxWidth += 120;
+  if (preloadText) {
     preloadText.style.width = textMaxWidth + "px";
-    index++;
-    if (index >= texts.length) {
-      clearInterval(textInterval);
 
-      setTimeout(() => {
-        preload.classList.add("loaded");
-        document.body.classList.remove("loading");
-      }, 300);
-    }
-  }, 600);
+    const textInterval = setInterval(() => {
+      preloadText.textContent = texts[index];
+      textMaxWidth += 120;
+      preloadText.style.width = textMaxWidth + "px";
+      index++;
+      if (index >= texts.length) {
+        clearInterval(textInterval);
+
+        setTimeout(() => {
+          preload.classList.add("loaded");
+          document.body.classList.remove("loading");
+        }, 300);
+      }
+    }, 600);
+  }
 });
 
 document.addEventListener("DOMContentLoaded", () => {
